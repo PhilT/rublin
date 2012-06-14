bfb1938000962c6db705c430894f50edf7503534
Phil Thompson <phil@electricvisions.com>
2012-06-14 10:10:33 +0100

    DRYd search/replace methods, Fixed ignore config

--- lib/search_replace.rb
+++ lib/search_replace.rb
@@ -7,28 +7,30 @@ class SearchReplace < Rublin
     @ignore = /(#{@config['ignore'].join('|')})/
   end
   
-  def self.search regexp
-    found = []
-    Dir["**/*"].each do |filename|
-      next if filename =~ @ignore || File.directory?(filename)
-      content = File.read(filename)
-      found << Pathname.new(filename).relative_path_from(Pathname.pwd) if content.scan(regexp).any?
-    end
-    found
+  def search regexp, file_pattern = nil
+    search_or_replace :in => file_pattern, :for => regexp
   end
 
-  def self.replace regexp, replacement
-    changed = []
+  def replace regexp, replacement, file_pattern = nil
+    search_or_replace :in => file_pattern, :for => regexp, :with => replacement
+  end
+  
+private
+  def search_or_replace options
+    pattern = options[:in] || '**/*'
+    search = options[:for]
+    replace = options[:with]
+    
+    matches = []
     Dir["**/*"].each do |filename|
       next if filename =~ @ignore || File.directory?(filename)
       content = File.read(filename)
-      new_content = content.gsub(regexp, replacement)
-      if content != new_content
-        File.open(filename, 'w') {|f| f.puts new_content }
-        changed << filename
+      if content.scan(search).any?
+        File.open(filename, 'w') {|f| f.puts content.gsub(search, replace) } if replace
+        matches << Pathname.new(filename).relative_path_from(Pathname.pwd)
       end
     end
-    changed
+    matches
   end
 end
 
--- spec/search_replace_spec.rb
+++ spec/search_replace_spec.rb
@@ -2,17 +2,41 @@ require 'spec_helper'
 
 describe 'SearchReplace' do
   before do
+    FileUtils.mkdir_p('config')
     FileUtils.mkdir_p('lib')
+    FileUtils.mkdir_p('tmp')
     FileUtils.mkdir_p('home')
+    File.open('config/search_replace.yml', 'w') {|f| f.puts ({'ignore' => ['tmp']}.to_yaml) }
     File.open('lib/source.rb', 'w') {|f| f.puts 'some source code in a file' }
     File.open('home/somefile', 'w') {|f| f.puts 'some text in a file' }
+    File.open('tmp/source.rb', 'w') {|f| f.puts 'some source code in a file' }
     File.open('home/anotherfile', 'w') {|f| f.puts 'non-matching text' }
   end
 
   describe 'search' do
     it 'finds matching string in files' do
-      matches = SearchReplace.search('some')
+      matches = SearchReplace.new.search('some')
       matches.must_equal [Pathname.new('home/somefile'), Pathname('lib/source.rb')]
+    end
+
+    it 'finds string in other file' do
+      matches = SearchReplace.new.search('matching')
+      matches.must_equal [Pathname.new('home/anotherfile')]
+    end
+  end
+  
+  describe 'replace' do
+    it 'replaces string in files' do
+      matches = SearchReplace.new.replace 'some', 'not much'
+      matches.must_equal [Pathname.new('home/somefile'), Pathname('lib/source.rb')]
+      File.read('home/somefile').must_equal "not much text in a file\n"
+      File.read('lib/source.rb').must_equal "not much source code in a file\n"
+    end
+    
+    it 'searches with a regex and replaces some text using grouping' do
+      matches = SearchReplace.new.replace /some (.*) in a/, '\1 in this'
+      File.read('home/somefile').must_equal "text in this file\n"
+      File.read('lib/source.rb').must_equal "source code in this file\n"
     end
   end
 end
